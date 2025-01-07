c52dd1f551cb19c22c9d064b66acb73464a638e0
jose luis riascos cubillos <joselriascos@unicauca.edu.co>
2025-01-07 17:19:40 -0500

    save board and turn in local storage

--- projects/02-tic-tac-toe/src/components/Board.jsx
+++ projects/02-tic-tac-toe/src/components/Board.jsx
@@ -7,8 +7,14 @@ import { checkWinnerFrom, checkEndGame } from '../logic/board'
 
 export function Board() {
 
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(() => {
+    const savedBoard = window.localStorage.getItem('board')
+    return savedBoard ? JSON.parse(savedBoard) : Array(9).fill(null)
+  })
+  const [turn, setTurn] = useState(() => {
+    const savedTurn = window.localStorage.getItem('turn')
+    return savedTurn ?? TURNS.X
+  })
   // null es que no hay ganador, false es empate
   const [winner, setWinner] = useState(null)
 
@@ -20,10 +26,16 @@ export function Board() {
     /* el Spread operator es util para hacer una copia superficial de
     arreglos como en este caso, si se necesitase hacer una copia profunda
     se podría hacer mediante la función structuredClone(array)*/
+
+    // si la casilla ya tiene algo o ya hay un ganador, no se hace nada
     if (board[index] || winner) return
+
+    // Actualizar el tablero
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
+
+    // Comprobar si hay un ganador
     const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
         confetti()
@@ -33,14 +45,23 @@ export function Board() {
         setWinner(false)
         return
     }
+
+    // Cambiar de turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    // Guadar partida en localStorage
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
   }
 
   const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
 
   return (
