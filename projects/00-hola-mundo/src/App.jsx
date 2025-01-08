6bd8adaf1e9a2d4b1a710095526b2dc4557ce350
jose luis riascos cubillos <joselriascos@unicauca.edu.co>
2025-01-07 19:09:22 -0500

    refactor:  added format to first project

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,38 +1,35 @@
-import './App.css';
-import { TwitterFollowCard  } from './TwitterFollowCard';
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
 
 export function App() {
+  // No es la mejor práctica, se usa cuando se necesita pasar muchas props
+  const elonmusk = {
+    userName: 'elonmusk',
+    name: 'Elon Musk',
+    isFollowing: true,
+  }
 
-    // No es la mejor práctica, se usa cuando se necesita pasar muchas props
-    const elonmusk = {
-        userName: 'elonmusk',
-        name: "Elon Musk",
-        isFollowing: true
-    }
+  const users = [
+    { userName: 'midudev', name: 'Miguel Ángel Durán', isFollowing: true },
+    { userName: 'pheralb', name: 'Pablo Hernandez', isFollowing: false },
+    elonmusk,
+    { userName: 'vanderhart', name: 'Vanderhart', isFollowing: false },
+  ]
 
-    const users = [
-        { userName: 'midudev', name: "Miguel Ángel Durán", isFollowing: true },
-        { userName: 'pheralb', name: "Pablo Hernandez", isFollowing: false },
-        elonmusk,
-        { userName: 'vanderhart', name: "Vanderhart", isFollowing: false }
-    ]
-
-    return (
-        <section className='App'>
-            {
-                users.map(user => {
-                    const { userName, name, isFollowing } = user
-                    return (
-                        <TwitterFollowCard
-                            key={userName}
-                            userName={userName}
-                            initialIsFollowing={isFollowing}
-                        >
-                            {name}
-                        </TwitterFollowCard>
-                    )
-                })
-            }
-        </section>
-    );
+  return (
+    <section className="App">
+      {users.map((user) => {
+        const { userName, name, isFollowing } = user
+        return (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        )
+      })}
+    </section>
+  )
 }
