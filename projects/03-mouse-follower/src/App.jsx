bc204c3988715b7f6b976149c7c3d1cb55e63ebd
jose luis riascos cubillos <joselriascos@unicauca.edu.co>
2025-01-08 15:07:50 -0500

    minor changes

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,10 +1,8 @@
-import { use } from 'react'
 import { useEffect, useState } from 'react'
 
 function App() {
-
-  const [ enabled, setEnabled ] = useState(false)
-  const  [ position, setPosition ] = useState({ x: 0, y: 0 })
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
 
   useEffect(() => {
     const handleMove = (event) => {
@@ -12,43 +10,44 @@ function App() {
       setPosition({ x: clientX, y: clientY })
     }
     if (enabled) {
-      window.addEventListener("pointermove", handleMove)
+      window.addEventListener('pointermove', handleMove)
     }
     return () => {
       // limpiar el efecto
       // Se ejecuta cuando el componente se desmonta
       // o cuando cambia la dependencia, antes de ejecutar el efecto
       // nuevamente
-      window.removeEventListener("pointermove", handleMove)
+      window.removeEventListener('pointermove', handleMove)
       setPosition({ x: 0, y: 0 })
     }
   }, [enabled])
 
   useEffect(() => {
-    document.body.classList.toggle("no-cursor", enabled)
+    document.body.classList.toggle('no-cursor', enabled)
     return () => {
-      document.body.classList.remove("no-cursor")
+      document.body.classList.remove('no-cursor')
     }
   }, [enabled])
 
   return (
     <>
       <main>
-        <div style={{
-          position: "absolute",
-          backgroundColor: "rgba(0, 0, 0, 0.4)",
-          borderRadius: "50%",
-          opacity: 0.8,
-          pointerEvents: "none",
-          left: -20,
-          top: -20,
-          width: 40,
-          height: 40,
-          transform: `translate(${position.x}px, ${position.y}px)`,
-        }}
+        <div
+          style={{
+            position: 'absolute',
+            backgroundColor: 'rgba(0, 0, 0, 0.4)',
+            borderRadius: '50%',
+            opacity: 0.8,
+            pointerEvents: 'none',
+            left: -20,
+            top: -20,
+            width: 40,
+            height: 40,
+            transform: `translate(${position.x}px, ${position.y}px)`,
+          }}
         />
         <button onClick={() => setEnabled(!enabled)}>
-          {enabled ? "Desactivar" : "Activar"} seguir puntero
+          {enabled ? 'Desactivar' : 'Activar'} seguir puntero
         </button>
       </main>
     </>
