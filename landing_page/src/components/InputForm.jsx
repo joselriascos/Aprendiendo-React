8d90ecf7792a9b54770060780ce24b794467018d
jose luis riascos cubillos <joselriascos@unicauca.edu.co>
2025-10-09 00:09:32 -0500

    removed landing page folder

--- landing_page/src/components/InputForm.jsx
+++
@@ -1,8 +0,0 @@
-export function InputForm({ text, type, required = false, id }) {
-  return (
-    <label htmlFor={id}>
-      {text}
-      <input type={type} id={id} required={required} />
-    </label>
-  )
-}
